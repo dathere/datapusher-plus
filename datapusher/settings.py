77b3ae6606bf8cdc262885328d81489a61401c1d
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-05-05 14:42:50 -0400

    Don't bother with sqlite
    we want a persistent job_store database
    
    - also format settings.py

--- datapusher/settings.py
+++ datapusher/settings.py
@@ -14,12 +14,18 @@ NAME = 'datapusher'
 HOST = os.environ.get('DATAPUSHER_HOST', '0.0.0.0')
 PORT = os.environ.get('DATAPUSHER_PORT', 8800)
 
-# Database
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATAPUSHER_SQLALCHEMY_DATABASE_URI', 'sqlite:////tmp/job_store.db')
+# job_store database
+# make sure to set this up properly by
+# 1) creating the job_store and datapusher_jobs user in PostgreSQL FIRST
+# 2) adjust the settings here, and
+# 3) run the INIT_DB command
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATAPUSHER_SQLALCHEMY_DATABASE_URI',
+                                         'SQLALCHEMY_DATABASE_URI=postgresql://datapusher_jobs:YOURPASSWORD@localhost/datapusher_jobs')
 
 # PostgreSQL COPY settings
 # set this to the same value as your ckan.datastore.write_url
-WRITE_ENGINE_URL = os.environ.get('DATAPUSHER_WRITE_ENGINE_URL', 'postgresql://datapusher:THEPASSWORD@localhost/datastore_default')
+WRITE_ENGINE_URL = os.environ.get(
+    'DATAPUSHER_WRITE_ENGINE_URL', 'postgresql://datapusher:THEPASSWORD@localhost/datastore_default')
 
 AUTO_ALIAS = bool(int(os.environ.get('DATAPUSHER_AUTO_ALIAS', '1')))
 
@@ -27,10 +33,12 @@ AUTO_ALIAS = bool(int(os.environ.get('DATAPUSHER_AUTO_ALIAS', '1')))
 QSV_BIN = os.environ.get('DATAPUSHER_QSV_BIN', '/usr/local/bin/qsvlite')
 QSV_DEDUP = bool(int(os.environ.get('DATAPUSHER_QSV_DEDUP', '1')))
 PREVIEW_ROWS = int(os.environ.get('DATAPUSHER_PREVIEW_ROWS', '10000'))
-DEFAULT_EXCEL_SHEET = int(os.environ.get('DATAPUSHER_DEFAULT_EXCEL_SHEET', '0'))
+DEFAULT_EXCEL_SHEET = int(os.environ.get(
+    'DATAPUSHER_DEFAULT_EXCEL_SHEET', '0'))
 
 # Download and streaming settings
-MAX_CONTENT_LENGTH = int(os.environ.get('DATAPUSHER_MAX_CONTENT_LENGTH', '1024000'))
+MAX_CONTENT_LENGTH = int(os.environ.get(
+    'DATAPUSHER_MAX_CONTENT_LENGTH', '1024000'))
 CHUNK_SIZE = int(os.environ.get('DATAPUSHER_CHUNK_SIZE', '16384'))
 CHUNK_INSERT_ROWS = int(os.environ.get('DATAPUSHER_CHUNK_INSERT_ROWS', '250'))
 DOWNLOAD_TIMEOUT = int(os.environ.get('DATAPUSHER_DOWNLOAD_TIMEOUT', '30'))
@@ -46,4 +54,4 @@ STDERR = bool(int(os.environ.get('DATAPUSHER_STDERR', '1')))
 # see jobs.py for their current default values
 # DATELIKE_FIELDNAMES
 # TYPE_MAPPING
-# TYPES 
+# TYPES
