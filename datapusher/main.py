3d32edff7e4144917b35cf39f59e2f61079e1e59
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-01-15 00:51:40 -0500

    apply black formatting

--- datapusher/main.py
+++ datapusher/main.py
@@ -5,12 +5,12 @@ from datapusher import jobs
 from config import config
 
 # check whether jobs have been imported properly
-assert(jobs.push_to_datastore)
+assert jobs.push_to_datastore
 
 
 def serve():
     web.init()
-    web.app.run(config.get('HOST'), config.get('PORT'))
+    web.app.run(config.get("HOST"), config.get("PORT"))
 
 
 def serve_test():
@@ -22,14 +22,19 @@ def main():
     import argparse
 
     argparser = argparse.ArgumentParser(
-        description='Service that allows automatic migration of data to the CKAN DataStore')
+        description="Service that allows automatic migration of data to the CKAN DataStore"
+    )
 
-    argparser.add_argument('config', metavar='CONFIG', type=argparse.FileType('r'),
-                           help='configuration file')
+    argparser.add_argument(
+        "config",
+        metavar="CONFIG",
+        type=argparse.FileType("r"),
+        help="configuration file",
+    )
 
     args = argparser.parse_args()
 
-    os.environ['JOB_CONFIG'] = os.path.abspath(args.config.name)
+    os.environ["JOB_CONFIG"] = os.path.abspath(args.config.name)
 
     serve()
 
@@ -37,20 +42,24 @@ def main():
 def initdb():
     import argparse
 
-    argparser = argparse.ArgumentParser(description='Initializes the database')
+    argparser = argparse.ArgumentParser(description="Initializes the database")
 
-    argparser.add_argument('config', metavar='CONFIG', type=argparse.FileType('r'),
-                           help='configuration file')
+    argparser.add_argument(
+        "config",
+        metavar="CONFIG",
+        type=argparse.FileType("r"),
+        help="configuration file",
+    )
 
     args = argparser.parse_args()
 
-    os.environ['JOB_CONFIG'] = os.path.abspath(args.config.name)
+    os.environ["JOB_CONFIG"] = os.path.abspath(args.config.name)
 
     import ckanserviceprovider.db as servicedb
 
     web._configure_app(web.app)
-    servicedb.init(config.get('SQLALCHEMY_DATABASE_URI'))
+    servicedb.init(config.get("SQLALCHEMY_DATABASE_URI"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
