52b62908de6920e7a8c2002219e1215e7abf66af
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-06-26 11:03:41 -0400

    0.14.1 release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.14.0',
+    version='0.14.1',
 
     description="A standalone web service that parses the contents of a CKAN site's data files (CSV, TSV, Excel and ODS) and pushes them into its DataStore. Accelerated by qsv.",
     long_description=long_description,
