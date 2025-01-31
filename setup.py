21804332891890ee1d1faa64d01218bba73659af
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-01-31 13:33:48 -0500

    deps: we need jinja2 to calculate scheming fields

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         # ``requirements.txt`` file.
         #
         # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
+        'jinja2>=3.1.5',
     ],
 
     # If there are data files included in your packages that need to be
